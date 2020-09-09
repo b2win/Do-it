4b4754b109044cffce248b8e03264f3e418a6eb1
b2win <68144560+b2win@users.noreply.github.com>
2020-09-09 12:41:56 +0900

    부록 온도변환 예제
    부록 온도변환 예제

---
+++ JavaScript/js/converter.js
@@ -0,0 +1,32 @@
+var sUnit = document.querySelector("#s-unit");
+
+var tUnit = document.querySelector("#t-unit");
+
+var cToF = true;
+
+var source = document.querySelector("#s-value");
+
+var target = document.querySelector("#t-value");
+
+function exUnit() {
+  source.value = "";
+  target.value = "";
+
+  if(cToF) {
+    cToF = false
+    sUnit.innerHTML = "&#8457;";
+    tUnit.innerHTML = "&#8451;";
+  } else {
+    cToF = true;
+    sUnit.innerHTML = "&#8451;";
+    tUnit.innerHTML = "&#8457;";
+  }
+}
+
+function converter() {
+  if(cToF) {
+    target.value = (source.value * 1.8 + 32).toFixed(2);
+  } else {
+    target.value = ((source.value - 32) / 1.8).toFixed(2);
+  }
+}
